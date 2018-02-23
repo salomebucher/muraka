5cbba989fb8fce637edf996924b2058eea645ac5
Salome <salomebucher@gmx.net>
2018-02-23 11:27:32 +0100

    mail setup

--- kontakt.php
+++ kontakt.php
@@ -1,26 +1,59 @@
 <?php
-/* === Daten aus dem Formular auslesen und in Variablen speichern === */
+session_cache_limiter('nocache');
+header('Expires: ' . gmdate('r', 0));
+header('Content-type: application/json');
 
-$name = 'Name' . $_POST['name2'];
-$email = 'Email' . $_POST['email2'];
-$nachricht = 'Nachricht' $_POST['message2'];
+$Recipient = 'info@murakadiving.com'; // <-- Set your email here
 
-/* === Empfängeradresse und Betreff === */
+if($Recipient) {
 
-$an = 'info@murakadiving.com'; //Hier Ihre E-Mail-Adresse eintragen
-$betreff = "Kontaktformularnachricht";
+	$Name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+	$Email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+	$Subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+	$Message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+	if (isset($_POST['guests'])) {
+		$Guests = filter_var($_POST['guests'], FILTER_SANITIZE_STRING);
+	} else {
+		$Guests = "";
+	}
+	if (isset($_POST['events'])) {
+		$Events = filter_var($_POST['events'], FILTER_SANITIZE_STRING);
+	} else {
+		$Events = "";
+	}
+	if (isset($_POST['category'])) {
+		$Category = filter_var($_POST['category'], FILTER_SANITIZE_STRING);
+	} else {
+		$Category = "";
+	}
 
+	$Email_body = "";
+	$Email_body .= "From: " . $Name . "\n" .
+				   "Email: " . $Email . "\n" .
+				   "Subject: " . $Subject . "\n" .
+				   "Message: " . $Message . "\n" .
+				   "No Of Guests: " . $Guests . "\n" .
+				   "Event: " . $Events . "\n" .
+				   "Category: " . $Category . "\n";
 
-/* === Wenn Bedingung erfüllt, dann E-Mail abschicken - andernfalls Fehlermeldung ausgeben === */
+	$Email_headers = "";
+	$Email_headers .= 'From: ' . $Name . ' <' . $Email . '>' . "\r\n".
+					  "Reply-To: " .  $Email . "\r\n";
 
-if ((!empty($name))&&(!empty($email))&&(!empty($nachricht))) {
+	$sent = mail($Recipient, $Subject, $Email_body, $Email_headers);
 
-mail($an, $betreff, $name . $nachricht, 'From:' . $email); //Mail versenden
+	if ($sent){
+		$emailResult = array ('sent'=>'yes');
+	} else{
+		$emailResult = array ('sent'=>'no');
+	}
 
-echo 'Ihre Kontaktnachricht wurde zugestellt. Sie werden bald möglichst eine Antwort erhalten.';
+	echo json_encode($emailResult);
+
+} else {
+
+	$emailResult = array ('sent'=>'no');
+	echo json_encode($emailResult);
+
 }
-
-else {
-echo 'Sie müssen alle Felder ausfüllen. <a href="about.html">Zurück</a>';
-}
-?> 
+?>
